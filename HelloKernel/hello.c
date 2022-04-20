455e172d9b95ae46928dcf013e693a8853fd181d
taqiyyuddinhammamafiify <43339144+taqiyyuddinhammamafiify@users.noreply.github.com>
2022-04-20 12:45:51 +0700

    Update hello.c

--- HelloKernel/hello.c
+++ HelloKernel/hello.c
@@ -5,18 +5,18 @@
 #include <linux/kernel.h>	/* Needed for KERN_INFO */
 #include <linux/init.h>		/* Needed for the macros */
 
-static int hello3_data __initdata = 3;
+static int hellokernel_data __initdata = 3;
 
-static int __init hello_3_init(void)
+static int __init hellokernel_init(void)
 {
 	printk(KERN_INFO "Hello, world %d\n", hello3_data);
 	return 0;
 }
 
-static void __exit hello_3_exit(void)
+static void __exit hellokernel_exit(void)
 {
 	printk(KERN_INFO "Goodbye, world 3\n");
 }
 
-module_init(hello_3_init);
-module_exit(hello_3_exit);
+module_init(hello_kernel);
+module_exit(hello_kernel);
