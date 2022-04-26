08b68610725bc7848f43cad85b5b728056d22679
taqiyyuddinhammamafiify <43339144+taqiyyuddinhammamafiify@users.noreply.github.com>
2022-04-26 12:13:24 +0700

    Update main.c

--- HelloKernel/app/main.c
+++ HelloKernel/app/main.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+#define device "/dev/HelloKernel"
+
+int i = 1;
+
+int main() {
+	FILE *fp;
+	char buffer[256];
+
+	printf("Demo calling kernel module using character device driver\n");
+	printf("Reading file %s...\n\n",device);
+
+        for(i; i<=2; i++){
+          fp = fopen(device,"r");
+	  if(fp == NULL) {
+  		printf("Can't open file %s\n",device);
+  		return 0;
+	  }
+
+          fread(buffer,sizeof(buffer),1,fp);
+          if(i==1)printf("Nama: %s\n",buffer);
+          if(i==2)printf("NIM : %s\n",buffer);        
+ 
+          fclose(fp);
+	}
+	return 0;
+}
